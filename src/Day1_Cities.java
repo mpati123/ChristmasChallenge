ecd8a453e1572bc74e989868d224e35f5d0e7ee2
Patrycja Nehrebecka <mpati123.pm@gmail.com>
2022-12-01 17:33:10 +0100

    Day 1 complete

--- src/Day1_Cities.java
+++ src/Day1_Cities.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.sqrt;
+
 public class Day1_Cities {
 
     /**
@@ -16,7 +22,34 @@ public class Day1_Cities {
 
     public static void main(String[] args) {
 
-        double neededPetrol = 0.0; //TODO: calculate
+        Places petrolStation = new Places(1, 1);
+        Places firstPlace = new Places(4, 5);
+        Places secondPlace = new Places(11, 5);
 
+        double distance = 0;
+        double neededPetrol = 0.0;
+
+        distance = calculateDistance(petrolStation, firstPlace);
+        distance += calculateDistance(firstPlace, secondPlace);
+        neededPetrol = calculatePetrol(distance);
+
+        System.out.println("Dystans Mikołaja do przebycia do ostatniego miejsca: " + distance);
+        System.out.println("Ilość benzyny którą potrzebuje zatankować aby dotrzeć do ostatniego miejsca: " + neededPetrol);
+
+        distance += calculateDistance(secondPlace, petrolStation);
+        neededPetrol = calculatePetrol(distance);
+
+        System.out.println("Dystans Mikołaja do przebycia wraz z powrotem do bazy: " + distance);
+        System.out.println("Ilość benzyny którą potrzebuje zatankować aby objechać wszystko i wrócić do bazy: " + neededPetrol);
+    }
+
+    private static double calculateDistance(Places initialPlace, Places finalPlace) {
+        double xDistance = abs(initialPlace.getX() - finalPlace.getX());
+        double yDistance = abs(initialPlace.getY() - finalPlace.getY());
+        return sqrt((xDistance * xDistance) + (yDistance * yDistance));
+    }
+
+    private static double calculatePetrol(double distance) {
+        return 2 * distance;
     }
 }
---
+++ src/Places.java
@@ -0,0 +1,17 @@
+public class Places {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Places(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
